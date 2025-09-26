afc3c23f5296353a56d05bb0089d1dd6c397c3bb
viju <avijusanjai@gmail.com>
2025-09-26 03:53:48 +0000

    working with replit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    svgr(),
-  ],
-})
+  plugins: [tailwindcss(), svgr()],
+  server: {
+    allowedHosts: [
+      "b8a105cc-a3e2-4581-885a-4c9dba782ede-00-3k8iwr9vkg4ri.janeway.replit.dev",
+    ],
+  },
+});
